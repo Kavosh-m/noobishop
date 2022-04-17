e3b0080fed30a7de110b8fe529b574906b739998
Kavosh Madadi <kavoshmadadiglk@gmail.com>
2022-04-18 00:09:44 +0430

    create map and change logo

--- src/routes/Contact.jsx
+++ src/routes/Contact.jsx
@@ -1,14 +1,67 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
+import { shopHeader } from "../constants";
+import mapboxgl from "mapbox-gl";
+
+mapboxgl.accessToken = process.env.REACT_APP_MAPBOX_TOKEN;
 
 const Contact = () => {
+  const mapContainer = useRef(null);
+  const map = useRef(null);
+  const [lng, setLng] = useState(2.37);
+  const [lat, setLat] = useState(48.83);
+  const [zoom, setZoom] = useState(9);
+
+  useEffect(() => {
+    if (map.current) return; // initialize map only once
+    map.current = new mapboxgl.Map({
+      container: mapContainer.current,
+      style: "mapbox://styles/mapbox/satellite-streets-v11",
+      center: [lng, lat],
+      zoom: zoom,
+      scrollZoom: false,
+    });
+
+    map.current.addControl(new mapboxgl.FullscreenControl());
+    map.current.addControl(
+      new mapboxgl.GeolocateControl({
+        positionOptions: {
+          enableHighAccuracy: true,
+        },
+        // When active the map will receive updates to the device's location as it changes.
+        trackUserLocation: true,
+        // Draw an arrow next to the location dot to indicate which direction the device is heading.
+        showUserHeading: true,
+      })
+    );
+
+    const nav = new mapboxgl.NavigationControl({
+      visualizePitch: true,
+    });
+    map.current.addControl(nav, "bottom-right");
+
+    // Create a new marker.
+    const marker = new mapboxgl.Marker({ color: "#ff0000" })
+      .setLngLat([lng, lat])
+      .setPopup(new mapboxgl.Popup().setHTML("<h1>Welcome to Noobishop</h1>"))
+      .addTo(map.current);
+    marker.togglePopup(); // toggle popup open or closed
+  });
+
   return (
     <div className="relative flex min-h-screen flex-col justify-between">
       <Navbar />
+      <div className="h-[375px] w-full bg-gray-300">
+        <img
+          src={shopHeader}
+          alt=""
+          className="h-full w-full object-cover brightness-50"
+        />
+      </div>
       <div className="flex-1 basis-auto bg-white">
         {/* Three square */}
-        <ul className="font-poppins mx-52 grid grid-cols-3 gap-8 py-10 text-sm">
+        <ul className="font-poppins mx-auto grid w-fit grid-cols-1 gap-8 py-10 text-sm sm:grid-cols-2 sm:grid-rows-2 lg:grid-cols-3 lg:grid-rows-1">
           <li className="group grid place-items-center border border-gray-300 bg-white p-10">
             <div>
               <section className="mx-auto w-fit rounded-full border border-red-300 p-3 transition-colors duration-300 ease-in-out group-hover:bg-red-300">
@@ -35,13 +88,13 @@ const Contact = () => {
               <p className="font-oswald py-4 text-center text-lg font-bold">
                 Our Location
               </p>
-              <p className="whitespace-nowrap text-center">
+              <p className="text-center">
                 (800) 123 456 789 / (800) 123 456 789
               </p>
               <p className="text-center">info@example.com</p>
             </div>
           </li>
-          <li className="group grid place-items-center border border-gray-300 bg-white">
+          <li className="group grid place-items-center border border-gray-300 bg-white p-10">
             <div>
               <section className="mx-auto w-fit rounded-full border border-red-300 p-3 transition-colors duration-300 ease-in-out group-hover:bg-red-300">
                 <svg
@@ -66,7 +119,7 @@ const Contact = () => {
               <p className="text-center">Fax: 123 456 789</p>
             </div>
           </li>
-          <li className="group grid place-items-center border border-gray-300 bg-white">
+          <li className="group grid place-items-center border border-gray-300 bg-white p-10 sm:col-span-2 lg:col-span-1">
             <div>
               <section className="mx-auto w-fit rounded-full border border-red-300 p-3 transition-colors duration-300 ease-in-out group-hover:bg-red-300">
                 <svg
@@ -118,6 +171,15 @@ const Contact = () => {
             placeholder="Message"
           />
         </div>
+        <button className="font-oswald ml-52 mt-10 w-fit whitespace-nowrap bg-red-400 px-7 py-3 text-center text-lg font-semibold text-white transition-colors duration-300 ease-in-out hover:bg-gray-300 hover:text-black">
+          SEND A MESSAGE
+        </button>
+
+        {/* Map */}
+        <div
+          ref={mapContainer}
+          className="mt-32 mb-20 h-[400px] bg-cyan-200"
+        ></div>
       </div>
       <Footer />
     </div>
