14861a9f35aa7dbb4623fa1fb252ebc4f2a0c5bc
Kavosh Madadi <kavoshmadadiglk@gmail.com>
2022-05-04 12:42:12 +0430

    responsive not found page

--- src/routes/NotFoundRoute.jsx
+++ src/routes/NotFoundRoute.jsx
@@ -1,13 +1,39 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Footer from "../components/Footer";
+import { shopHeader } from "../constants";
 
 const NotFoundRoute = () => {
   return (
-    <div>
-      <p>Page not found</p>
-      <Link to="/" className="mt-5 rounded-3xl bg-red-200 px-4 py-2">
-        Back to Home
-      </Link>
+    <div className="relative flex min-h-screen flex-col justify-between">
+      <div className="h-[375px] w-full bg-gray-300">
+        <img
+          src={shopHeader}
+          alt=""
+          className="h-full w-full object-cover brightness-50"
+        />
+      </div>
+      <div className="flex-1 basis-auto bg-white">
+        <div className="mx-auto my-8 flex max-w-full flex-col items-center space-y-6 bg-white sm:max-w-[80%] md:max-w-[70%] lg:max-w-[45%]">
+          <h1 className="font-oswald text-center text-6xl font-bold text-red-400 sm:text-9xl">
+            404
+          </h1>
+          <h3 className="font-oswald text-center text-xl font-medium sm:text-2xl">
+            OPPS! PAGE NOT BE FOUND
+          </h3>
+          <p className="font-poppins px-4 text-center text-sm font-normal text-slate-600 sm:px-0 sm:text-base">
+            Sorry but the page you are looking for does not exist, have been
+            removed, name changed or is temporarily unavailable.
+          </p>
+          <Link
+            to="/"
+            className="font-poppins rounded-[3px] bg-red-400 px-4 py-2 text-center text-xs font-medium uppercase text-white transition-colors duration-300 ease-in-out hover:bg-black"
+          >
+            back to home page
+          </Link>
+        </div>
+      </div>
+      <Footer />
     </div>
   );
 };
